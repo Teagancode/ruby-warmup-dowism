304525ab80cdd3e35c710934f4a973ea20a011ce
Seth Dow <sdow@sbacademy.org>
2016-11-29 19:47:57 -0800

    Add files via upload

---
+++ Vocab_list.rb
@@ -0,0 +1,16 @@
+=begin
+this is how you comment in ruby, using begin and end,
+you can also use a hash tag for shorter comments
+for this little project you are going to start a vocab list
+where you are going to set all the vocab equal to variables of the same name.
+
+=end
+pred = "this makes a integer go down one, you use it like 5.pred would be 4"#this is a comment
+variable = "A variable is the name that you assign to a piece of information"
+integer =
+boolean_value =
+reverse
+string
+terminal commands 
+operators =
+method =
